588ebee140fe47b7c90eb26cb6d3a970259f3341
Abraão C Cassinda <elavokokassinda@gmail.com>
2023-09-14 17:52:51 -0300

    atualização do angular

--- src/main/java/br/ind/cmil/gestao/GestaoApplication.java
+++ src/main/java/br/ind/cmil/gestao/GestaoApplication.java
@@ -1,12 +1,7 @@
 package br.ind.cmil.gestao;
 
-import br.ind.cmil.gestao.model.dto.PerfilDTO;
-import br.ind.cmil.gestao.model.services.interfaces.IPerfilService;
-import java.util.stream.Stream;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GestaoApplication {
